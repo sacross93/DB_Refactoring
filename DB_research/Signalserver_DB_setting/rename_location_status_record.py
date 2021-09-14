d97c7b4cb9c55a57ceee86282b7adc9dcf899d2c
sacross93 <68053552+sacross93@users.noreply.github.com>
2021-09-14 14:44:45 +0900

    insert project

--- DB_research/Signalserver_DB_setting/rename_location_status_record.py
+++ DB_research/Signalserver_DB_setting/rename_location_status_record.py
@@ -17,10 +17,11 @@ test_DB_cursor = test_db.cursor(pymysql.cursors.DictCursor)
 #     id          int primary key auto_increment,
 #     date        timestamp not null,
 #     location_id int       not null,
-#     status      int       not null,
+#     status_id      int       not null,
 #     foreign key (location_id) references location (id),
-#     foreign key (status) references status (id),
-#     index time_idx (date)
+#     foreign key (status_id) references status (id),
+#     index time_idx (date),
+#     index status_idx (status_id)
 # );
 # """
 
@@ -33,4 +34,25 @@ from sa_api_bed_status_record;
 test_DB_cursor.execute(sql)
 status_list = pd.DataFrame(test_DB_cursor.fetchall())
 
-status_list
+
+for i,j,k in zip(status_list['date'],status_list['bed'],status_list['status']) :
+    bed_sql = \
+    f"""
+    select id
+    from location
+    where name = '{j}';
+    """
+    test_DB_cursor.execute(bed_sql)
+    bed_id=test_DB_cursor.fetchall()
+    if 'reen' in k:
+        status=1
+    elif 'Blue' in k :
+        status=2
+    else :
+        status=3
+    insert_sql= \
+    f"""
+    insert into location_status_record (date,location_id,status)
+    values ('{i}',{bed_id[0]['id']},{status})
+    """
+    test_DB_cursor.execute(insert_sql)

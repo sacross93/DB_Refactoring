54b06c097a57493b8498fba2501297c5b3111218
sacross93 <68053552+sacross93@users.noreply.github.com>
2021-09-17 15:38:45 +0900

    etc update

--- request_data_insert.py
+++ request_data_insert.py
@@ -1,4 +1,18 @@
-import pymysql
+import os
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
+
+npz_list = os.listdir("/srv/data/MBP_data")
+
+for i in npz_list :
+    test_npz = np.load(f"/srv/data/MBP_data/{i}")
+    # print(len(test_npz['data']))
+    if len(test_npz['data']) == 0 :
+        print(i)
+    # plt.figure(figsize=(20,10))
+    # plt.plot(test_npz['time'],test_npz['data'])
+    # plt.show()
+    # plt.close()
+
+len(npz_list)
